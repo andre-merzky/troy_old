eee51b88aa56e7b2049aa35eba195fba3655e767
Mark Santcroos <mark@santcroos.net>
2012-08-27 09:47:00 +0200

    Comments for discussion.

--- troy/__init__.py
+++ troy/__init__.py
@@ -17,6 +17,7 @@ Overview:
       utilization -- a workplan.  That workplan includes information about the
       required subset of resources, the kind of work planned, and any additional
       relevant or irrelevant information.
+      [ MS: Is the introduction of a 'workplan' really necessary? ]
     
       The resource manager can either decline to enact the requested workplan, or to
       enact it at some point in time, on some subset of its resources.  That
@@ -48,6 +49,7 @@ Overview:
   (instances of pilot frameworks), and supports the application level scheduling
   of workplans across those pilot systems (note that this Troy level scheduling
   process may in turn consist of multiple subprocesses, i.e.  subschedulers).
+  [ MS: I don't think the subschedulers add much clarity)
   Once the Troy level schedulers are done, the workplans are forwarded to the
   underlying pilot systems for further scheduling and enactment.  Any scheduler on
   Troy level may perform complete scheduling, i.e. schedule the workplan for
@@ -129,9 +131,12 @@ Terminology:
         Troy level sub scheduler, manager by the troy.Scheduler.
   
       - PilotFramework: 
-        representation of any pilot system, exposing the UnitService
-        (DataUnitService, ComputeUnitService, and ComputeDataUnitService) and
-        PilotService (DataPilotService and ComputePilotService) semantics.
+        representation of an instance of any pilot system, exposing the
+        UnitService (DataUnitService, ComputeUnitService, and
+        ComputeDataUnitService) and PilotService (DataPilotService and
+        ComputePilotService) semantics.
+        [ MS: Does it really expose all these semantics? If so, why do we need
+        a different concept? ]
   
       - ComputePilotDescription: as in P* / Pilot API
       - DataPilotDescription:    as in P* / Pilot API
@@ -151,6 +156,7 @@ Hello World:
   
   A simple Troy application scheduling a Compute centric workplan would be (on
   'Hello World' level, some details left out for brevity)::
+  [ MS: Please complete, hello world examples are by definition complete ]
   
       ----------------------------------------------------------------
       import troy
@@ -160,20 +166,30 @@ Hello World:
   
       # add a Round Robin scheduling algorithm
       s = troy.Scheduler ('round_robin')
+      # [ MS: This looks weird. I guess I would expect something like
+      # troy.Scheduler.RoundRobin or troy.RoundRobinScheduler probably ]
       t.add_scheduler (s)
+      # [ MS: Given that it is called "add", and not "select", does this mean
+      # there can be multiple schedulers? ]
   
       # create two ComputePilots from a BigJob PilotFramework
       pf = troy.PilotFramework ('bigjob://')
       pf.submit_pilot ([some compute pilot description])
       pf.submit_pilot ([some compute pilot description])
+      # [ MS: You think its a good idea to have one submit_pilot call for both
+      # data and compute? ]
   
       # add the bigjob PF to Troy, to submit work items to
       t.add_pilot_framework (pf)
+      # [ MS: And the pilot job framework will announce its capabilities to the
+      # system? ]
   
       # submit a compute work item
       cu = t.submit_unit ([some compute unit description])
+      # [ MS: Similar to submit pilot, good idea to have one? ]
   
       cu.wait ()     # wait 'til work is done
+
   
       pf.cancel ()   # terminate compute pilots and bigjob
       t.cancel ()    # terminate Troy
@@ -229,6 +245,9 @@ from troy.data_unit_description          import DataUnitDescription
 from troy.compute_unit                   import ComputeUnit
 from troy.data_unit                      import DataUnit
 
+
+# [ MS: The splitting of class:file (1:1) seems to be too rigorous and not
+# really pythonesqueue, but I'll leave that up to you ]
 
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
 

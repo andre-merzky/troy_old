a0d0b6a4cda7e7e948e9e8b8c2ace7cb3158a4ba
Ashley Z <anz7@rutgers.edu>
2013-05-30 11:15:52 -0400

    Tested being able to change adaptors, appears to work now #2

---
+++ examples/experimental/testing.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+
+import troy
+import time
+import pdb
+
+PF_URL = "bigjob+redis://ILikeBigJob_wITH-REdIS@gw68.quarry.iu.teragrid.org:6379"
+#PF_URL = "bigjob+redis://localhost:6379"
+
+def test_compute ():
+    # try:
+        print " 1 -------------------------------------------- "
+        t   = troy.Troy ()
+        print " 2 -------------------------------------------- "
+        pf  = troy.PilotFramework (PF_URL)
+
+        print " 3 -------------------------------------------- "
+        t.add_pilot_framework (pf)
+
+        print " 4 -------------------------------------------- "
+        s   = troy.Scheduler ('Catsaresupergreat')
+        print "CATS"
+        print str(s)
+        t.add_scheduler (s)
+
+        print " 5 -------------------------------------------- "
+        cpd  = troy.ComputePilotDescription ()
+
+        cpd.service_url         = 'fork://localhost'
+        cpd.working_directory   = '/home/azebro1/.bigjob/agent'
+        cpd.number_of_processes = 1
+
+
+        print " 6 -------------------------------------------- "
+        cp1  = pf.submit_pilot (cpd)
+
+        print " 7 -------------------------------------------- "
+        cp2  = pf.submit_pilot (cpd)
+
+        print " 8 -------------------------------------------- "
+
+        for id in t.list_pilot_frameworks () :
+            print "pf: " + str (id) + " : "
+            print "    " + str (troy.PilotFramework(id).list_pilots())
+
+        print " 9 -------------------------------------------- "
+        cud  = troy.ComputeUnitDescription ()
+
+        cud['executable'] = '/bin/touch'
+        cud['arguments']  = ['/tmp/hello_troy_pj']
+
+        print " 10 ------------------------------------------- "
+        cu  = t.submit_unit (cud)
+
+        print " 11 ------------------------------------------- "
+        s_  = cu.state
+
+        while s_ != troy.State.Done and \
+              s_ != troy.State.Failed   :
+
+            print "cu : %s"  %  (str(s_))
+            time.sleep (1)
+          # from pudb import set_trace; set_trace()
+            s_ = cu.state
+
+        print "cu : %s"  %  (str(s_))
+
+        print " 11 -------------------------------------------- "
+        cp1.cancel ()
+        cp2.cancel ()
+        print " 12 ------------------------------------------- "
+        pf.cancel ()
+ 
+    # except Exception, e:
+    #     print str (e)
+
+
+ 
+def main():
+    test_compute ()
+
+if __name__ == '__main__':
+    main()
+
---
+++ troy/adaptors/troy_adaptor_scheduler_cats.py
@@ -0,0 +1,121 @@
+
+import imp
+import random
+
+import troy
+import troy.interface
+from   troy.adaptors.base import aBase
+from   troy.exception     import Exception, Error
+
+
+########################################################################
+# 
+# This TROY adaptor implements a simple random scheduler, which schedules 
+# (as the above suggests) 
+#   
+# Note: http://xkcd.com/221/
+
+
+########################################################################
+#
+# main adaptor class, registers the implementation with troy upon loading
+#
+class adaptor (aBase) :
+    
+    def __init__ (self) :
+        # duh!
+        self.name     = 'troy_adaptor_scheduler_cats'
+
+        # The registry maps api interface classes to the adaptor classes 
+        # implementing them:
+        self.registry = { 'Scheduler' : 'scheduler_cats' }
+        
+
+    def sanity_check (self) :
+        # raise troy.Exception (Error.NoSuccess, "adaptor disabled")
+        
+        # what did you expect?
+        # if sys.random (1) > 0.5 :
+        #     return True
+        # else :
+        #     return False
+        pass
+    
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+# Random Scheduler
+#
+
+########################################################################
+class scheduler_cats (troy.interface.iScheduler) :
+
+    def __init__ (self, api, adaptor) :
+
+        self.api     = api 
+        self.adaptor = adaptor
+
+        # we MUST interpret policy, if present
+        if 'policy' in self.api._idata :
+            if self.api._idata['policy'] != 'Cats' :
+                raise troy.Exception (troy.Error.NoSuccess,
+                      "Cannot provide the requested scheduling policy!")
+
+    def ud_is_compute (self, ud) :
+        if 'executable' in ud :
+            return True
+        return False
+
+    def ud_is_data (self, ud) :
+        if 'urls' in ud :
+            return True
+        return False
+
+
+    def schedule (self, t, ud) :
+        pf_ids = t.list_pilot_frameworks ()
+        pilots = []
+        print "MEOW!!!  You're in the cat scheduler!!!"
+        exit(0)
+
+        # gather all available pilots
+        for pf_id in pf_ids :
+            print "PILOT_FRAMEWORK"
+            print pf_id
+            pf    = troy.PilotFramework (pf_id)
+            p_ids = pf.list_pilots ()
+            print "have pf: %s" % pf
+
+            for p_id in p_ids :
+                print "PILOT"
+                print p_id
+                try :
+                    if self.ud_is_compute (ud) :
+                        p = troy.ComputePilot (p_id)
+                        print p
+                        pilots.append (p)
+                    elif self.ud_is_data (ud) :
+                        pilots.append (troy.DataPilot (p_id))
+                    else :
+                        # ignore invalid ud's
+                        pass
+                except Exception as e:
+                    # print str(e)
+                    # ignore invalid pilot IDs (think race condition)
+                    pass
+
+
+        if len (pilots) == 0 :
+            raise troy.Exception (troy.Error.IncorrectState,
+                  "Cannot schedule: no pilots available")
+
+        # select a random pilot
+        idx = random.randint (0, len  (pilots) - 1)
+        p   = pilots[idx]
+
+        # and attempt to submit
+        return p.submit_unit (ud)
+
+
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+
